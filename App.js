8f025e0966a1678a6a1d9dda2e00985584927c6b
Kaj Białas <kaj.bialas@dev-industry.com>
2023-11-30 08:23:14 +0100

    Change app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Counter from "./views/counter/Counter";
 import Todos from "./views/todos/Todos";
 import TodoDetails from "./views/todoDetails/TodoDetails";
-
+import Ionicons from "react-native-vector-icons/Ionicons";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -20,10 +20,24 @@ const CounterNavigator = () => (
         <Stack.Screen name="Counter" component={Counter} />
     </Stack.Navigator>
 )
+
+const iconsNormal = {
+    'Todos': 'ios-list',
+    'Counter': 'ios-timer'
+}
 export default function App() {
   return (
     <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Navigator screenOptions={({ route}) => {
+            return ({
+                headerShown: false,
+                tabBarIcon: ({color, size}) => {
+                    const iconName = iconsNormal[route.name];
+                    return (<Ionicons name={iconName} color={color} size={size} />)
+
+                }
+            })
+        }}>
             <Tab.Screen name="Todos" component={TodoNavigator} />
             <Tab.Screen name="Counter" component={CounterNavigator} />
         </Tab.Navigator>
