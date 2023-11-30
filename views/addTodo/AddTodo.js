7a8da9a1807180fc4d1ef88258fa4a8d8518f0d2
Kaj Białas <kaj.bialas@dev-industry.com>
2023-11-30 12:52:59 +0100

    Add todo

--- views/addTodo/AddTodo.js
+++ views/addTodo/AddTodo.js
@@ -1,21 +1,28 @@
-import { SafeAreaView, Text, Button } from "react-native";
+import { SafeAreaView, Text, Button, TextInput } from "react-native";
 import { useTodos } from "../../context/todo.contex";
-
+import { useState } from "react";
 // Zadanie 3: Utwórz formularz dodawania nowych todo, pole userId narazie niech będzie 1
 const AddTodo = () => {
     const { addTodo } = useTodos();
+    const [title, setTitle] = useState('');
 
     const handleAddTodo = () => {
         addTodo({
-            id: 'dsfgdfsg',
-            title: 'Zrobić zakupy',
+            id: Date.now(),
+            title: title,
             userId: 1,
             completed: false
-        })
+        });
+        setTitle('')
     }
     return (
         <SafeAreaView>
             <Text>Add Todo</Text>
+            <TextInput
+                value={title}
+                onChangeText={setTitle}
+                placeholder="Nazwa zadania"
+            />
             <Button title="Add todo" onPress={handleAddTodo} />
         </SafeAreaView>
     )
