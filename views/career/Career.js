fd442f9bdab871c8a3f5b48b0ee17a4400e4159f
Kaj Białas <kaj.bialas@dev-industry.com>
2023-12-01 13:33:09 +0100

    Change text button

--- views/career/Career.js
+++ views/career/Career.js
@@ -1,17 +1,46 @@
-import { View} from "react-native";
+import { useRef } from 'react';
+import { View, SafeAreaView} from "react-native";
 import { WebView } from 'react-native-webview';
+import { Button } from "native-base";
 
+// Zadanie 3a
+// Utwórz podstronę draft menu: o nas, oraz zbuduj strukture html
 const Career = () => {
+    const webViewRef = useRef(null);
+    const templateString = `
+        <div>
+            <h1>Tytuł</h1>
+            <article>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet ligula molestie, maximus quam id, ultricies ante. Duis venenatis odio elit. Phasellus vestibulum consectetur purus cursus dignissim. In arcu metus, sodales nec nibh vel, vehicula tincidunt felis. Donec a neque a ligula placerat tempor. Integer consequat diam ipsum, sit amet iaculis diam fermentum sed. In condimentum enim eget ipsum tempus iaculis. Proin tristique vulputate risus in molestie. Fusce enim lacus, condimentum vitae ante sed, vehicula vestibulum ipsum. Aliquam efficitur dui sit amet dapibus euismod. Aenean et porta tellus, eu molestie libero.
+            </article>
+        </div>
+    `;
+
+    const increaseTextSize = () => {
+        webViewRef.current.injectJavaScript(
+            `
+               document.body.style.fontSize = '50px';
+            `
+        )
+    }
+
+    // Zadanie 5
+    // Utwórz przyciski zwiększ oraz zmniejsz wpływające na wielkość webview
+
     return (
-        <View style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1}}>
             <WebView
-                source={{uri: 'https://www.accenture.com/pl-en/careers/jobsearch?jk=&sb=1&vw=0&is_rj=0&pg=7&ba=technology&jt=experienced'}}
+                ref={webViewRef}
+                source={{html: templateString}}
                 style={{flex: 1}}
                 javaScriptEnabled={true}
                 domStorageEnabled={true}
                 scalesPageToFit={true}
             />
-        </View>
+            <Button onPress={increaseTextSize}>
+                Zwieksz
+            </Button>
+        </SafeAreaView>
     )
 }
 
